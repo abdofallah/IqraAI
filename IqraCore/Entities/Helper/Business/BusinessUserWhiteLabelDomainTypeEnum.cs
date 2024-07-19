7f6f8fe931e979c91edfdd01387c6bb6df11e6b9
abdullahbaa5 <abdudsoul@gmail.com>
2024-07-19 11:09:06 +0400

    basic sub users tab implementation

---
+++ IqraCore/Entities/Helper/Business/BusinessUserWhiteLabelDomainTypeEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace IqraCore.Entities.Helper.Business
+{
+    public enum BusinessUserWhiteLabelDomainTypeEnum
+    {
+        Unknown = 0,
+        IqraSubdomain = 1,
+        CustomDomain = 2
+    }
+}
